d2ca0d59a238964e0ac766dce514027272750a17
Jai96 <jai96@example.com>
2025-05-23 11:44:10 +0900

    [Setting] 카카오 로그아웃 기능, 카카오 정보 가져오기 추가 Fixes #36

--- lib/presentation/pages/setting/widget/setting_tile.dart
+++ lib/presentation/pages/setting/widget/setting_tile.dart
@@ -4,13 +4,27 @@ class SettingTile extends StatelessWidget {
   final String title;
   final VoidCallback onTap;
 
-  const SettingTile({super.key, required this.title, required this.onTap});
+  const SettingTile({
+    super.key,
+    required this.title,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(title),
-      trailing: const Icon(Icons.chevron_right),
+      tileColor: const Color(0xFFFFFFFF),
+      title: Text(
+        title,
+        style: const TextStyle(
+          color: Color(0xFF222222),
+          fontSize: 16,
+        ),
+      ),
+      trailing: const Icon(
+        Icons.chevron_right,
+        color: Color(0xFF222222),
+      ),
       onTap: onTap,
     );
   }
